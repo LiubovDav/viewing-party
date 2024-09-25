e78a9a4c89c94215815074d6bfb990ba78eb990a
Liubov Dav <lubasha0512@gmail.com>
2024-09-24 23:23:45 -0700

    Implemented wave_02 functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -12,17 +12,20 @@ def create_movie(title, genre, rating):
 
     return movie
 
+
 def add_to_watched(user_data, movie):
 
     user_data["watched"].append(movie)
 
     return user_data
 
+
 def add_to_watchlist(user_data, movie):
 
     user_data["watchlist"].append(movie)
 
     return user_data
+
 
 def watch_movie(user_data, title):
 
@@ -66,12 +69,52 @@ def watch_movie(user_data, title):
 
 
 
-
-    
-
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+
+    if len(user_data["watched"]) == 0:
+        return 0.0
+    
+    sum = 0
+
+    for movie in user_data["watched"]:
+        sum += movie["rating"]
+    
+    return sum / len(user_data["watched"])
+
+
+def get_most_watched_genre(user_data):
+
+    if len(user_data["watched"]) == 0:
+        return None
+
+    genres_count = {}
+
+    # genres_count = {
+    #     "Fantasy": 12,
+    #     "Action": 5,
+    #     ...
+    # }
+
+    for movie in user_data["watched"]:
+        genre = movie["genre"]
+        if genre in genres_count:
+            genres_count[genre] += 1
+        else:
+            genres_count[genre] = 1
+    
+    max_count = 0
+    most_frequently_watched_genre = ""
+
+    for genre in genres_count.keys():
+        if genres_count[genre] > max_count:
+            max_count = genres_count[genre]
+            most_frequently_watched_genre = genre
+    
+    return most_frequently_watched_genre
+
 
 
 # -----------------------------------------
